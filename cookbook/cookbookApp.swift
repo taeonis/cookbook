a801555c2401e9190f49d5f36602e6049259c0ec
teonei <taeoninorgaar@gmail.com>
2025-08-05 14:00:16 -0700

    Initial Commit

---
+++ cookbook/cookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookbookApp.swift
+//  cookbook
+//
+//  Created by Taeoni Norgaar on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct cookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cookbookTests/cookbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  cookbookTests.swift
+//  cookbookTests
+//
+//  Created by Taeoni Norgaar on 8/5/25.
+//
+
+import Testing
+@testable import cookbook
+
+struct cookbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
